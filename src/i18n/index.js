c36633addbd32037233df2d4c62cae7b2d8c0912
shusinthebox <shusinthebox@gmail.com>
2020-03-22 01:17:37 +0900

    Add in configuration for i18n translation

---
+++ src/i18n/index.js
@@ -0,0 +1,25 @@
+import i18n from 'i18next';
+import Backend from 'i18next-xhr-backend';
+import { initReactI18next } from 'react-i18next';
+
+i18n
+  .use(Backend)
+  .use(initReactI18next)
+  .init({
+    lng: 'jp',
+    backend: {
+      loadPath: '/locales/{{lng}}.json',
+    },
+    fallbackLng: 'en',
+    ns: ['translations'],
+    defaultNS: 'translations',
+    interpolation: {
+      escapeValue: false,
+      formatSeparator: ',',
+    },
+    react: {
+      wait: true,
+    },
+  });
+
+export default i18n;
