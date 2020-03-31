0e8d8c6633f62271cf78527f4e448abcbe1dac6e
shusinthebox <shusinthebox@gmail.com>
2020-03-31 22:36:13 +0900

    Hack for specifically local dev and github page deployment

--- src/app/index.js
+++ src/app/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from '../store';
 import Navigation from './navigation';
 import Routes from './routes';
+
 function App() {
   return (
     <Provider store={store}>
-      <HashRouter basename="/">
+      <Router path={process.env.PUBLIC_URL}>
         <Navigation />
         <Routes />
-      </HashRouter>
+      </Router>
     </Provider>
   );
 }
